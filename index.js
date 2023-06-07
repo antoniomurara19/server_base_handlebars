41d17842793c9be6f31d96b8d00612326d51896d
antonio murara zabala <antoniomurar4@gmail.com>
2023-06-07 12:32:00 -0300

    loja de frutas

--- index.js
+++ index.js
@@ -19,25 +19,42 @@ app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs.engine())
 
 /* --------------------------------------- */
-
-
-app.post('/pesquisar', async(res,req)=>{
+app.get('/apagar',(req,res)=>{
+    res.render('apaga')
+})
+app.post('/apagar', async(req,res)=>{
+    const id = req.body.id
+    console.log(id)
+    const pesq = await Verdura.findOne({raw:true,where:{id:id}})
+    console.log(pesq)
+    Verdura.destroy({where: {id:pesq.id}})
+    res.redirect('/listar')
+})
+app.get('/pesquisar',(req,res)=>{
+    res.render('pesquisa')
+})
+app.post('/pesquisar', async(req,res)=>{
     const codigo = req.body.codigo
     console.log(codigo)
     const pesq = await Verdura.findOne({raw:true,where:{id:codigo}})
     console.log(pesq)
-    res.redirect('/')
+    res.render('pesquisa',{valor:pesq})
 })
-app.get('/pesquisar',(req,res)=>{
-    res.render('pequisa')
+app.get('/cadastrar',(req,res)=>{
+    res.render('cadastro')
 })
-
+app.post('/cadastrar', async(req,res)=>{
+    const verduras = req.body.verduras
+    const qntd = req.body.qntd
+    const preco_unit = req.body.preco_unit
+    console.log(verduras,qntd,preco_unit)
+    await Verdura.create({verduras,qntd,preco_unit})
+    res.redirect('/listar')
+})
 app.get('/listar', async (req,res)=>{
     const dados = await Verdura.findAll({raw:true}) 
     res.render('lista',{valores:dados})
 })
-
-
 app.get('/',(req,res)=>{
     res.render('home')
 })
